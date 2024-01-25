1269ac779c0e7895b7ac619c5c3422153ce668a7
krzemian <1569895+krzemian@users.noreply.github.com>
2024-01-25 23:56:57 +0100

    Pass all tests in 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...valuesToRemove) {
+    return array.filter((el) => valuesToRemove.indexOf(el) == -1);
 };
 
 // Do not edit below this line
