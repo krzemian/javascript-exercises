8b4c4d0cac468d734d013d267ccb1da026f75bf1
krzemian <1569895+krzemian@users.noreply.github.com>
2024-01-26 00:10:13 +0100

    Pass al tests for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    if ((from < 0 || to < 0) ||
+        (!Number.isInteger(from) || !Number.isInteger(to))) return 'ERROR';
+    if (from > to) {
+        let fromBckp = from;
+        from = to;
+        to = fromBckp;
+    }
 
+    let sum = 0;
+    for (let i = from; i <= to; i++) sum += i;
+
+    return sum;
 };
+
+//  n + (n-1) + (n-2) + ... + (x+1) + x
 
 // Do not edit below this line
 module.exports = sumAll;
